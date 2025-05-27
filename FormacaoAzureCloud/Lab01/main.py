3473708357063e1c503aea7c600030c087e08cc9
FelipeFioreti <felipe.fioretirosolen@gmail.com>
2025-05-27 18:25:52 -0300

    Adicionando melhorias ao projeto

--- FormacaoAzureCloud/Lab01/main.py
+++ FormacaoAzureCloud/Lab01/main.py
@@ -17,7 +17,7 @@ SQL_DATABASE = os.getenv("SQL_DATABASE")
 SQL_USER = os.getenv("SQL_USER")
 SQL_PASSWORD = os.getenv("SQL_PASSWORD")
 
-st.title("Casdastro de Produtos")
+st.title("Cadastro de Produtos")
 
 #Register form product
 product_name = st.text_input("Nome do Produto")
@@ -48,12 +48,85 @@ def insert_product(product_name, product_price, product_description, product_ima
         st.error(f"Erro ao inserir produto: {e}")
         return False
 
+def list_products():
+    try:
+        conn = pymssql.connect(server=SQL_SERVER, user=SQL_USER, password=SQL_PASSWORD, database=SQL_DATABASE)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM Produtos")
+        products = cursor.fetchall()
+        conn.close()
+        return products
+    except Exception as e:
+        st.error(f"Erro ao listar produtos: {e}")
+        return []
+
+def list_products_screen():
+    products = list_products()
+    if products:
+
+        cards_por_linha = 3
+        cols = st.columns(cards_por_linha)
+
+        for i, product in enumerate(products):
+            col = cols[i % cards_por_linha]
+            with col:
+                st.write(f"**Nome:** {product[1]}")
+                st.write(f"**Descrição:** {product[2]}")
+                st.write(f"**Preço:** R$ {product[3]:.2f}")
+                if product[4]:
+                    html_image = f'<img src="{product[4]} " width="200" height="200" alt="Imagem do Produto">'
+                    st.markdown(html_image, unsafe_allow_html=True)
+                st.markdown("---")
+            if( i + 1) % cards_por_linha == 0 and (i + 1) < len(products):
+                cols = st.columns(cards_por_linha)
+    else:
+        st.write("Nenhum produto cadastrado.")
+
+def update_product():
+    try:
+        if product_id_update is None or product_id_update == "":
+            st.error("Por favor, informe o ID do produto a ser atualizado.")
+            return
+
+        if product_image_update is None:
+            st.error("Por favor, faça o upload de uma nova imagem do produto.")
+            return
+        
+        image_url_update = upload_blob(product_image_update) 
+        conn = pymssql.connect(server=SQL_SERVER, user=SQL_USER, password=SQL_PASSWORD, database=SQL_DATABASE)
+        cursor = conn.cursor()
+        cursor.execute(f"UPDATE Produtos SET Nome = '{product_name_update}', Descricao = '{product_description_update}', Preco = {product_price_update}, Imagem_URL = '{image_url_update}' WHERE ID = {product_id_update}")
+        conn.commit()
+        conn.close()
+        st.success("Produto atualizado com sucesso!")
+    except Exception as e:
+        st.error(f"Erro ao atualizar produto: {e}")
+
 if st.button("Salvar Produto"):
     insert_product(product_name,product_price,product_description,product_image)
     return_message = "Produto salvo com sucesso"
 
 st.header("Produtos Cadastrados")
 
-if(st.button("Listar Produtos")):
+if st.button("Listar Produtos"):
+    list_products_screen()
     return_message = "Produtos listados com sucesso"
 
+st.header("Atualizar Produto")
+
+# Update form product
+
+product_id_update = st.text_input("ID do Produto a ser atualizado")
+product_name_update = st.text_input("Novo Nome do Produto")
+product_price_update = st.number_input("Novo Preço do Produto", min_value=0.0, format="%.2f")
+product_description_update = st.text_area("Nova Descrição do Produto")
+product_image_update = st.file_uploader("Nova Imagem do Produto", type=["jpg","png", "jpeg"])
+
+if st.button("Atualizar Produtos"):
+    update_product()
+    product_id_update = None
+    product_name_update = ""
+    product_price_update = 0.0
+    product_description_update = ""
+    product_image_update = None
+    return_message = "Produto atualizado com sucesso"
